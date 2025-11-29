d0ba98d2a037e52a4da23befdb04a1512137789f
Msi <calugas.jonathan.18@gmail.com>
2025-11-29 19:59:27 +0800

    created nested dynamic paths

---
+++ app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,9 @@
+export default async function ProductReview({params}:{
+  params: Promise<{productId: string, reviewId:string}>
+}){
+  const {productId, reviewId} = await params;
+  return <>
+    <h1>Review {reviewId}</h1>
+    <h1>Product {productId}</h1>
+  </>
+}
